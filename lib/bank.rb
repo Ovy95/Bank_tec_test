c280a34ad36553a6aa63b3fd00423f00d98da8fb
Ovy95 <jackoverton21@hotmail.com>
2020-04-15 15:51:37 +0100

    Deposit & Withdraw Methods added

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,11 +1,16 @@
 class Bank 
+  attr_reader :balance
   def initialize
     @balance = 0 
   end
 
-  def balance
-    @balance 
+  def deposit(amount)
+    @balance += amount
   end
+
+  def withdraw(amount)
+    @balance -= amount
+  end 
 
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,12 +1,21 @@
 require "bank"
 
 describe Bank do 
+  let(:bank) {Bank.new}
 
-
-  it  "Returns the balance of zero" do 
-    bank = Bank.new
+  it "Returns the balance of zero" do 
     expect(bank.balance).to eq 0
   end 
+
+  it "User can deposit 1000 to the account " do 
+    bank.deposit(1000)
+    expect(bank.balance).to eq 1000
+  end
+
+  it "User can withdraw 500 from the account" do
+    bank.withdraw(500)
+    expect(bank.balance).to eq -500
+  end
 
 end
 
